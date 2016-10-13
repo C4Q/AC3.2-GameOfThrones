1437519194f326c0a1e44b5899197a03e28801db
Marty Hernandez Avedon <martyavedon@gmail.com>
2016-10-13 14:38:03 -0400

    testing if the git repos are set up correctly

--- AC3.2-GameOfThrones/GameOfThronesTableViewController.swift
+++ AC3.2-GameOfThrones/GameOfThronesTableViewController.swift
@@ -12,6 +12,8 @@ class GameOfThronesTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //test
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
