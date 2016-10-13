aa80eb178361e9d554095dfa91682556786df654
Marty Hernandez Avedon <martyavedon@gmail.com>
2016-10-13 15:56:24 -0400

    setting up cells, still wip, getting my favorite app delegate error

--- AC3.2-GameOfThrones/GameOfThronesTableViewController.swift
+++ AC3.2-GameOfThrones/GameOfThronesTableViewController.swift
@@ -40,32 +40,28 @@ class GameOfThronesTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return episodes.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "eppy", for: indexPath)
 
         // Configure the cell...
+        
+        
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
