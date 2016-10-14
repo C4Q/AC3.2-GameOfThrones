99d5c68fa2784c7d055f0587eb558a40a50e6cfa
Marty Hernandez Avedon <martyavedon@gmail.com>
2016-10-14 11:34:38 -0400

    styling cells and setting up a separate cell file

---
+++ selectEpisodeCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  selectEpisodeCellTableViewCell.swift
+//  AC3.2-GameOfThrones
+//
+//  Created by Marty Avedon on 10/14/16.
+//  Copyright © 2016 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class selectEpisodeCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
